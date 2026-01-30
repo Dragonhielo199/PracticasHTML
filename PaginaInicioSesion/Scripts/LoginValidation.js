bbfc8c10d583a1a224186461d1ee34ec2cb74c2f
atejedormar <hydra851gt@gmail.com>
2026-01-30 11:24:02 +0100

    Move PaginaInicio and PaginaInicioStyle to a new folder. Add to PaginaInicioSesion a new error message.

--- PaginaInicioSesion/Scripts/LoginValidation.js
+++ PaginaInicioSesion/Scripts/LoginValidation.js
@@ -6,12 +6,27 @@ e.preventDefault();
 					
 	let user = document.getElementById("usuario").value;
 	let pass = document.getElementById("clave").value;
+	
+	const mensajeUsuario = document.getElementById("mensajeErrorUsuario");
+    const mensajeContrasena = document.getElementById("mensajeErrorContrasena");
 					
 	let indice = usuarios.indexOf(user);
+	
+	if (indice === -1) {
+        mensajeUsuario.style.display = "block";
+        setTimeout(() => {
+            mensajeUsuario.style.display = "none";
+        }, 3000);
+    }
+	
+	if (contrasenas[indice] !== pass) {
+        mensajeContrasena.style.display = "block";
+        setTimeout(() => {
+            mensajeContrasena.style.display = "none";
+        }, 3000);
+    }
 
 	if (indice !== -1 && contrasenas[indice] === pass) {
 		alert("Inicio de sesión correcto");
-	} else {
-		alert("Usuario o contraseña incorrectos");
-	}
+	} 
 });
